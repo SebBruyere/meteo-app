dbf716ad10c3b874939080c388d812a13f7e0648
Zbeb <zbeb@me.com>
2020-07-04 11:39:31 +0200

    Creating forecast block

---
+++ src/components/App/Forecast/Forecast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Forecast.css';
+
+function Forecast() {
+  return (
+    <div className="component-forecast">
+        <h1>Week forecast</h1>
+    </div>
+  );
+}
+
+export default Forecast;
