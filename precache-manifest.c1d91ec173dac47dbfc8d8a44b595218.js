5c2f71e35158e9b123cfbaf67789ae18a087d84e
Zbeb <zbeb@me.com>
2020-07-08 19:43:52 +0200

    Updates

---
+++ precache-manifest.c1d91ec173dac47dbfc8d8a44b595218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e77cec16a629b1d78248550957cf8de",
+    "url": "/meteo-appp/index.html"
+  },
+  {
+    "revision": "f825ef0e12c94603fd96",
+    "url": "/meteo-appp/static/css/main.2ff8d76a.chunk.css"
+  },
+  {
+    "revision": "e26790a1d481ff758950",
+    "url": "/meteo-appp/static/js/2.bc66a0c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meteo-appp/static/js/2.bc66a0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f825ef0e12c94603fd96",
+    "url": "/meteo-appp/static/js/main.12594e69.chunk.js"
+  },
+  {
+    "revision": "83c56fe2a47a11180ec2",
+    "url": "/meteo-appp/static/js/runtime-main.01852aa9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/meteo-appp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
