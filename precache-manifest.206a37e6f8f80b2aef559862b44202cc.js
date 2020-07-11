59987cb8e2b282b86cae37af3e38abeb0f499b91
Zbeb <zbeb@me.com>
2020-07-11 18:13:48 +0200

    Updates

---
+++ precache-manifest.206a37e6f8f80b2aef559862b44202cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e304114789be257ef0e36f19790bf08d",
+    "url": "/meteo-app/index.html"
+  },
+  {
+    "revision": "d508c06423addf52c9d4",
+    "url": "/meteo-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d1b41e37f1f8039fcc6d",
+    "url": "/meteo-app/static/css/main.55345a76.chunk.css"
+  },
+  {
+    "revision": "d508c06423addf52c9d4",
+    "url": "/meteo-app/static/js/2.ac15ec5e.chunk.js"
+  },
+  {
+    "revision": "f863024c2d9d2f2bdd781cf3f534e630",
+    "url": "/meteo-app/static/js/2.ac15ec5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b41e37f1f8039fcc6d",
+    "url": "/meteo-app/static/js/main.c76e4d64.chunk.js"
+  },
+  {
+    "revision": "2f82020020ab3e6fc9f8",
+    "url": "/meteo-app/static/js/runtime-main.1d9be2da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meteo-app/precache-manifest.bfea6381904bb25778c76fd635beb6c8.js"
+  "/meteo-app/precache-manifest.206a37e6f8f80b2aef559862b44202cc.js"
 );
 
 self.addEventListener('message', (event) => {
